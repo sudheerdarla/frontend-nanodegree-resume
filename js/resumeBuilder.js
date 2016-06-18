291b3a2ae4707571ac7cd26f171bbfc63b99cacd
Sudheer Darla <sudheer.darla11@gmail.com>
2016-06-19 00:43:04 +0530

    added string manipulation code

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,14 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- // $("#main").append("sudheer");
 
-// var awesomethoughts = "I am sudheer, and udacity is awesome!";
-// var funthoughts = awesomethoughts.replace("awesome", "fun");
-// $("#main").append(funthoughts);
 
-// console.log(awesomethoughts);
-// console.log(funthoughts);
 
 
 var formattedName = HTMLheaderName.replace("%data%", "Sudheer");
@@ -17,18 +11,70 @@ var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 
-// var s = "audacity";
 
-// var udacityizer = function(s) {  
-//     // Right now, the variable s === "audacity"
-//     // Manipulate s to make it equal to "Udacity"
-//     // Your code goes here!
-//     s = s.slice(1);
-//     s = s.replace("u", "U");
-    
-//     return s;
-// };
 
-// // Did your code work? The line below will tell you!
-// console.log(udacityizer(s));
+
+
+
+// quizz
+
+  // $("#main").append("sudheer");
+
+// quizz
+
+	// var awesomethoughts = "I am sudheer, and udacity is awesome!";
+	// var funthoughts = awesomethoughts.replace("awesome", "fun");
+	// $("#main").append(funthoughts);
+
+	// console.log(awesomethoughts);
+	// console.log(funthoughts);
+
+// quizz
+
+	// var s = "audacity";
+
+	// var udacityizer = function(s) {  
+	//     // Right now, the variable s === "audacity"
+	//     // Manipulate s to make it equal to "Udacity"
+	//     // Your code goes here!
+	//     s = s.slice(1);
+	//     s = s.replace("u", "U");
+	    
+	//     return s;
+	// };
+
+	// // Did your code work? The line below will tell you!
+	// console.log(udacityizer(s));
+
+
+// quizz
+
+	// var name = "slbERt EINstEiN";
+
+	// function nameChanger(oldName) {
+	//     var finalName = oldName;
+	//     // Your code goes here!
+
+	//     var space = finalName.indexOf(" ");
+
+	//     var firstWord = finalName.slice(0, space).toLowerCase();
+
+	//     var firstLetter = firstWord.slice(0, 1).toUpperCase();
+	//     firstWord = firstLetter + firstWord.slice(1, space);
+	    
+	//     var secondWord = finalName.slice(space);
+	//     secondWord = secondWord.toUpperCase();
+
+	//     finalName = firstWord + secondWord;
+	//     // Don't delete this line!
+	//     return finalName;
+	// };
+
+	// // Did your code work? The line below will tell you!
+	// console.log(nameChanger(name));
+
+
+
+
+
 
