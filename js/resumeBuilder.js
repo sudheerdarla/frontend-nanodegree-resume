adba979e92915372f49519fe4cb5502c4456551c
Sudheer Darla <sudheer.darla11@gmail.com>
2016-06-19 23:34:44 +0530

    added 4 objects and quizzes.js file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,77 +1,75 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
 
 
+var work = {
+	"jobs" : [
+		{
+			"employer" : "Fiverr",
+			"title" : "Web Developer",
+			"Location" : "Home",
+			"dates" : 2015,
+			"description" : "Started my freelancing career in this year!"	
+		},
+		{
+			"employer" : "TestEmployer",
+			"title" : "test Developer",
+			"Location" : "Home1",
+			"dates" : 2016,
+			"description" : "Already started my freelancing career in this year!"
+		}
+	]
+}
 
 
-var formattedName = HTMLheaderName.replace("%data%", "Sudheer");
-var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
-
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-
-
-
-
-
-
-// quizz
-
-  // $("#main").append("sudheer");
-
-// quizz
-
-	// var awesomethoughts = "I am sudheer, and udacity is awesome!";
-	// var funthoughts = awesomethoughts.replace("awesome", "fun");
-	// $("#main").append(funthoughts);
-
-	// console.log(awesomethoughts);
-	// console.log(funthoughts);
-
-// quizz
-
-	// var s = "audacity";
-
-	// var udacityizer = function(s) {  
-	//     // Right now, the variable s === "audacity"
-	//     // Manipulate s to make it equal to "Udacity"
-	//     // Your code goes here!
-	//     s = s.slice(1);
-	//     s = s.replace("u", "U");
-	    
-	//     return s;
-	// };
-
-	// // Did your code work? The line below will tell you!
-	// console.log(udacityizer(s));
+var projects = {
+	"project" : [
+		{
+			"title" : "Web Developer",
+			"dates" : 2015,
+			"description" : "Started my freelancing career in this year!",
+			"images" : "url('http://spacecenter.org/wp-content/uploads/2013/04/Apollo-Soyuz-BG-2000x2000.jpg')";
+		},
+		{
+			"title" : "Web Developer",
+			"dates" : 2016,
+			"description" : "Started my freelancing career in this year!",
+			"images" : "url('https://c1.staticflickr.com/1/414/19752043426_166097bd18_b.jpg')";
+		}
+	]
+}
 
 
-// quizz
+var bio = {
+	"name" : "Sudheer",
+	"role" : "Web Developer",
+	"welcomeMessage" : "Hello Udacity!",
+	"Contacts" : {
+		"mobile" : "99999999",
+		"email" : "test@testmail.com",
+		"github" : "github.io/sud",
+		"twitter" : "twitter.com/sud",
+		"Location" : "Hyderabad"
+	},
+	"skills" : ["Web development, Designing"]
+}
 
-	// var name = "slbERt EINstEiN";
 
-	// function nameChanger(oldName) {
-	//     var finalName = oldName;
-	//     // Your code goes here!
+var education = {
+	"schools": [{
+		"citasdfies": "Manufdsguru",
+		"majasdfors": ["Diploma in Electrical"],
+		"minofdsrs": "others",
+		"grasdfdyears": 2014
+	}, {
+		"cities": "Manuaasdfguru",
+		"majors": ["Diploma in Electrical", "college dropout"],
+		"minors": "othsdfers",
+		"gradyears": 201334
+	}],
 
-	//     var space = finalName.indexOf(" ");
-
-	//     var firstWord = finalName.slice(0, space).toLowerCase();
-
-	//     var firstLetter = firstWord.slice(0, 1).toUpperCase();
-	//     firstWord = firstLetter + firstWord.slice(1, space);
-	    
-	//     var secondWord = finalName.slice(space);
-	//     secondWord = secondWord.toUpperCase();
-
-	//     finalName = firstWord + secondWord;
-	//     // Don't delete this line!
-	//     return finalName;
-	// };
-
-	// // Did your code work? The line below will tell you!
-	// console.log(nameChanger(name));
+	"online courses": [{
+		"courses": ["CS50", "Udacity", "Udemy", "Learn Python the Hard way etc.,"]
+	}]
+}
 
 
 
---
+++ quizzes.js
@@ -0,0 +1,122 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+
+
+// var formattedName = HTMLheaderName.replace("%data%", "Sudheer");
+// var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+// $("#header").prepend(formattedRole);
+// $("#header").prepend(formattedName);
+
+
+
+
+
+// quizz
+
+	// var work = {
+	// 	"position" : "Learner",
+	// 	"employer" : "Web-Developer",
+	// 	"yearsworked" : 2
+	// };
+
+	// work.city = "Hyderabad";
+	// work.business = "Freelancer";
+
+
+	// var education = {
+	// 	"name" : " World's University",
+	// 	"yearsattended" : 3
+	// };
+
+	// education["city"] = "Manuguru";
+
+	// $("#header").append(work["position"]);
+	// $("#header").append(education.name);
+
+// quizz
+
+	// var bio = {
+	//   "name" : "Sudheer",
+	//   "role" : "Learner",
+	//   "message" : "Hello, Udacity",
+	//   "phone" : "0011001100"
+	// };
+	// console.log(bio.name);
+	// $("#main").prepend(bio.name);
+
+
+
+// quizz
+
+  // $("#main").append("sudheer");
+
+// quizz
+
+	// var awesomethoughts = "I am sudheer, and udacity is awesome!";
+	// var funthoughts = awesomethoughts.replace("awesome", "fun");
+	// $("#main").append(funthoughts);
+
+	// console.log(awesomethoughts);
+	// console.log(funthoughts);
+
+// quizz
+
+	// var s = "audacity";
+
+	// var udacityizer = function(s) {  
+	//     // Right now, the variable s === "audacity"
+	//     // Manipulate s to make it equal to "Udacity"
+	//     // Your code goes here!
+	//     s = s.slice(1);
+	//     s = s.replace("u", "U");
+	    
+	//     return s;
+	// };
+
+	// // Did your code work? The line below will tell you!
+	// console.log(udacityizer(s));
+
+
+// quizz
+
+	// var name = "slbERt EINstEiN";
+
+	// function nameChanger(oldName) {
+	//     var finalName = oldName;
+	//     // Your code goes here!
+
+	//     var space = finalName.indexOf(" ");
+
+	//     var firstWord = finalName.slice(0, space).toLowerCase();
+
+	//     var firstLetter = firstWord.slice(0, 1).toUpperCase();
+	//     firstWord = firstLetter + firstWord.slice(1, space);
+	    
+	//     var secondWord = finalName.slice(space);
+	//     secondWord = secondWord.toUpperCase();
+
+	//     finalName = firstWord + secondWord;
+	//     // Don't delete this line!
+	//     return finalName;
+	// };
+
+	// // Did your code work? The line below will tell you!
+	// console.log(nameChanger(name));
+
+
+	// 2nd version
+
+	// function nameChanger(oldName) {
+	//     var finalName = oldName;
+	//     var names = oldName.split(" ");
+	//     names[1] = names[1].toUpperCase();
+	//     names[0] = names[0].slice(0,1).toUpperCase() + names[0].slice(1).toLowerCase();
+	//     finalName = names.join(" ");
+	//     return finalName;
+	// }
+
+
+
+
